3b1b5b75b99d2efba8ec7555a3daae581a2d130e
tenacious <fbeachler@gmail.com>
2016-10-21 00:52:46 -0600

    update metadata short desc

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'dfu-util'
 maintainer       'Free Beachler'
 maintainer_email 'longevitysoft@gmail.com'
 license          'Apache 2.0'
-description      'Installs/Configures gcc-arm-embedded'
+description      'Installs/Configures dfu-util from the sf.net repository.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 depends          'git'
