97d441c0f91d46ac5cb5770a46604ceef01311c2
tenacious <fbeachler@gmail.com>
2016-10-21 00:53:10 -0600

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'longevitysoft@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures dfu-util from the sf.net repository.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 depends          'git'
 depends          'apt'
 
